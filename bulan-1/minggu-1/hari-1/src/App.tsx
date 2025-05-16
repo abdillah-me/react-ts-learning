ee8a5a3b862354dfc42daffac3eab6b819eb61db
abdillah-me <muhammad.abdillah@idstar.group>
2025-05-16 13:25:54 +0700

    uji husky

--- bulan-1/minggu-1/hari-1/src/App.tsx
+++ bulan-1/minggu-1/hari-1/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+                 <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -18,14 +18,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+                  <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit  <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p        className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
