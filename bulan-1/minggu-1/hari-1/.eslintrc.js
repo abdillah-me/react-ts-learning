81a076e34ba9b1a19eabfc5b23636f247ab18ee8
abdillah-me <muhammad.abdillah@idstar.group>
2025-05-16 14:32:28 +0700

    update commit day 1

--- bulan-1/minggu-1/hari-1/.eslintrc.js
+++ bulan-1/minggu-1/hari-1/.eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  plugins: ['react', '@typescript-eslint'],
-  parserOptions: {
-    ecmaFeatures: { jsx: true },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  rules: {
-    'react/react-in-jsx-scope': 'off', // Vite tidak butuh ini
-  },
-  settings: {
-    react: { version: 'detect' },
-  },
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+    ],
+    plugins: ['react', '@typescript-eslint'],
+    parserOptions: {
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    rules: {
+        'react/react-in-jsx-scope': 'off', // Vite tidak butuh ini
+    },
+    settings: {
+        react: { version: 'detect' },
+    },
 }
