652e20991c6b301f02fb5bba2573ca7dbcab083a
abdillah-me <muhammad.abdillah@idstar.group>
2025-05-16 13:29:30 +0700

    uji husky

--- bulan-1/minggu-1/hari-1/src/App.tsx
+++ bulan-1/minggu-1/hari-1/src/App.tsx
@@ -15,12 +15,18 @@ function App() {
         </a>
         <a href="https://react.dev" target="_blank">
           <img  src={reactLogo} className="logo react" alt="React logo" />
+
+
         </a>
       </div>
       <h1>Vite + React</h1>
+
+
       <div className="card">
+
+        
                   <button onClick={() => setCount((count) => count + 1)}>
-                    
+
           count is {count}
         </button>
         <p>
